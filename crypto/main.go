d246be7b26cc21287c151404494ff3b48beaa954
Eugen <eugen.kusowenko@fmc-ag.com>
2020-03-19 15:05:56 +0000

    Functions added

--- crypto/main.go
+++ crypto/main.go
@@ -1,121 +1,31 @@
 package main
 
-import "fmt"
-
-func main() {
-
-	fmt.Println("Hello, Gopher!")
-
-}
-3. Writes file names in a .txt
-// 4. Saves .txt in new folder
-package main
-
 import (
+	"crypto/sha256"
 	"fmt"
-	"io/ioutil"
-	"os"
 )
 
-func main() {
-
-	// Loop over the files in path return the len of each file
-	// and add it to total; +1 as additional place for line separator '\n'
-
-	// Create new Folder in dir
-	dirName := "newdir"
-	path := "./" + dirName
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		os.Mkdir(path, 0744)
-		fmt.Println("newdir successfully created")
-	} else {
-		fmt.Println("newdir already exsists")
-	}
-	// Read Files in dir
-	files, err := ioutil.ReadDir(".")
-	if err != nil {
-		fmt.Println("Something went wrong! - I will return from the function!")
-		return
-	}
-
-	// To optimize the program the backing array is created only once
-	// Therefore the size of the backing array needs to be calculated
-	// var total is the capacity of the backing array
-
-	var total int
-	for _, file := range files {
-		if file.Name() != "main.go" || file.Name() != dirName {
-			total += len(file.Name()) + 1
-		}
-	}
-
-	// names var is declared wiht the make func
-	// only bytes are written to files
-	names := make([]byte, 0, total)
-	// Loop over the files, check for size and append the names
-	for _, file := range files {
-		if file.Name() != "main.go" && file.Name() != dirName {
-			n := file.Name()
-			names = append(names, n...)
-			// '\n' seperator needed to separate the bytes by line
-			names = append(names, '\n')
-		}
-	}
-	// '0644' is a Unix code for file permissions
-	err = ioutil.WriteFile(dirName+"/out.txt", names, 0644)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// for _, v := range names {
-	// 	fmt.Println(v.Name())
-	// }
-
-	// Declare a fileNames var of type slice by using the make func of len(files-1)
-	// fileNames := make([]string, len(files)-1)
-	// fmt.Printf("%#v \n", fileNames)
-
-	// for i := 0; i < len(files)-1; i++ {
-	// 	if files[i].Name() != "main.go" {
-	// 		fileNames[i] = files[i].Name()
-	// 	}
-	// }
-	// fmt.Printf("%#v \n", names)
-
-	// Create .txt file to write the fileNames to:
-	var txt = "File_Names.txt"
-
-	// check if file exists
-	// var _, err = os.Stat(txt)
-
-	// create file if not exists
-	// if os.IsNotExist(err) {
-	// 	var newFile, err = os.Create(txt)
-	// 	if isError(err) {
-	// 		return
-	// 	}
-	// 	defer newFile.Close()
-	// }
-
-	fmt.Println("File Created Successfully", txt)
-
+func createKey() [32]byte {
+	passPhrase := "Some Pass Phrase"
+	key := sha256.Sum256([]byte(passPhrase))
+	return key
 }
 
-// txt is only declare for the func main() scope--> cannot be used for other functions:
+func getFiles() []byte {
+	plain := []byte("exampleplaintext")
+	return plain
+}
 
-// func deleteFile() {
-// 	// delete file
-// 	var err = os.Remove(txt)
-// 	if isError(err) {
-// 		return
-// 	}
+// func encrypt() {
+// 	block := cipher.NewGCM()
 // }
 
-// func isError(err error) bool {
-// 	if err != nil {
-// 		fmt.Println(err.Error())
-// 	}
+func main() {
 
-// 	return (err != nil)
-// }
+	key := createKey()
+
+	plain := getFiles()
+
+	fmt.Printf("%x \n", key)
+	fmt.Printf("%x \n", plain)
+}
