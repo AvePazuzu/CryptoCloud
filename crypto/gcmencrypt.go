e1110c1b143092f066c2646bcc18049dda3bc1bf
Eugen <eugen.kusowenko@fmc-ag.com>
2020-03-25 10:55:33 +0000

    GCM added

--- crypto/gcmencrypt.go
+++ crypto/gcmencrypt.go
@@ -21,19 +21,20 @@ func main() {
 		panic(err.Error())
 	}
 
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	aesgcm.NonceSize()
 	// Never use more than 2^32 random nonces with a given key because of the risk of a repeat.
 	nonce := make([]byte, 12)
 	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
 		panic(err.Error())
 	}
 
-	aesgcm, err := cipher.NewGCM(block)
-	if err != nil {
-		panic(err.Error())
-	}
-
 	ciphertext := aesgcm.Seal(nil, nonce, text, nil)
 
+	fmt.Printf("%d \n", aesgcm.NonceSize())
 	fmt.Printf("%s\n", text)
 	fmt.Printf("%s encoded: %x\n", text, ciphertext)
 
