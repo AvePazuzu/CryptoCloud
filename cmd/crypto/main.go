77f621577d57a604d2980eaf3846aa46022cf5d9
Eugen <e.kusowenko@gmail.com>
2020-03-28 18:28:48 +0100

    session log created

--- cmd/crypto/main.go
+++ cmd/crypto/main.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"runtime"
 	"time"
 )
 
 func main() {
 
-	fmt.Println(time.Now())
-
 	// set start
 	t1 := time.Now()
+
+	// create session log
+	sessionLog := make([]byte, 0)
+	// append time stamp
+	sessionLog = append(sessionLog, "Date: "+t1.Format("02.01.2006 15:04:05")...)
+	// append operating system
+	sessionLog = append(sessionLog, "\n"+"OS: "+runtime.GOOS...)
 
 	// retrieve the files to encrypt
 	fileNames := getFiles()
@@ -34,9 +40,18 @@ func main() {
 		decrypt(name)
 	}
 
-	// set end and calculate the elapsed time
+	// set end, calculate the elapsed time and append it to sessionLog
 	t2 := time.Now()
 	elapsed := t2.Sub(t1)
 	fmt.Println(elapsed)
-	fmt.Println(runtime.GOOS)
+	sessionLog = append(sessionLog, "\n"+"Elapsed Time: "+elapsed.String()...)
+
+	// write session log to file
+	// '0644' is a Unix code for file permissions
+	err := ioutil.WriteFile("./sessionlog.txt", sessionLog, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 }
